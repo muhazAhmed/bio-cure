4dc3ce194bbe94593ce8199f444c63eb04de7c4e
muhazAhmed <muhazvla313@gmail.com>
2024-09-04 20:35:32 +0530

    fixed bable errors

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: "jsdom",
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
   moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
   testPathIgnorePatterns: ["/node_modules/", "/.next/"],
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
-  },
 };
--- src/pages/__tests__/index.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import HomePage from "../pages/index";
-
-test("renders welcome message", () => {
-  render(<HomePage />);
-  expect(screen.getByText("Welcome to BioCure")).toBeInTheDocument();
-});
