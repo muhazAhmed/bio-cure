326e3a25d84f8acbcdcbb9b3171fcf3534633394
muhazAhmed <muhazvla313@gmail.com>
2024-09-13 20:32:11 +0530

    added global colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        csBlue: "#1D76EA",
+        csLightBlue: "#E1EEFF",
+      }
     },
   },
   plugins: [],
