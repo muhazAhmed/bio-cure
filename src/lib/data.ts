bce85b4ad8dc5f4214214ee5d4253ec6f8b0a689
muhazAhmed <muhazvla313@gmail.com>
2024-09-13 20:32:56 +0530

    added new data list

---
+++ src/lib/data.ts
@@ -0,0 +1,6 @@
+export const navItems = [
+    {label: "Home", link: "/", value: 0},
+    {label: "About", link: "/about", value: 1},
+    {label: "Contact Us", link: "/contact", value: 2},
+    {label: "Application", link: "/application", value: 3},
+]
