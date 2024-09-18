0a77de373f6c85af2a3462f57740eb229cd737a5
muhazAhmed <muhazvla313@gmail.com>
2024-09-18 23:48:57 +0530

    added nextApiReq types

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,3 +4,14 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+import { NextApiRequest } from 'next';
+
+declare module 'next' {
+    interface NextApiRequest {
+        user?: {
+            userId: string;
+            role: string;
+        };
+    }
+}
